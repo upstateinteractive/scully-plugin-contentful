94345ff359a43ff2dfba859253e74f51176beeae
Peter Smith <peter@upstateinteractive.io>
2020-02-20 07:31:47 -0500

    correct public-api

--- projects/scully-plugin-contentful/src/public-api.ts
+++ projects/scully-plugin-contentful/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of scully-plugin-contentful
  */
 
-export * from './lib/scully-plugin-contentful.service';
-export * from './lib/scully-plugin-contentful.component';
-export * from './lib/scully-plugin-contentful.module';
+export { Contentful } from './lib/index';
